922cf0cc0a74522252d541c60560546b823f25d9
UTTAM-VAGHASIA <the.uttam.vaghasia@gmail.com>
2025-06-22 21:01:10 +0530

    Made the code modular

--- fluttercraft/main.py
+++ fluttercraft/main.py
@@ -1,31 +1,11 @@
-from turtle import width
 import typer
 from rich.console import Console
-from rich.panel import Panel
-import pyfiglet
 
 from fluttercraft.commands.start import start_command
-from fluttercraft import __version__
 from fluttercraft.utils.display_utils import display_welcome_art
 
 app = typer.Typer(help="FlutterCraft: Automate your Flutter app setup like a pro.")
 console = Console()
-
-
-def display_welcome_art():
-    """Display the FlutterCraft ASCII art and welcome message."""
-    art = pyfiglet.figlet_format("Flutter Craft", font="banner3-D")
-    console.print(
-        Panel.fit(
-            f"[bold cyan]{art}[/]\n"
-            "[bold green]Automate your Flutter app setup like a pro.[/]\n"
-            "[yellow]From folder structure to backend integration, from icons to "
-            "GitHub repo setup — FlutterCraft does it all.[/]",
-            border_style="blue",
-            title="Welcome to FlutterCraft 🛠️🚀",
-            subtitle=f"v{__version__}"
-        )
-    )
 
 
 @app.command()
