8208e8393614a4a522ba5cc8d5f4201900832ddb
UTTAM-VAGHASIA <the.uttam.vaghasia@gmail.com>
2025-06-23 17:54:00 +0530

    Bumped to version 0.1.2.dev1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open("README.md", encoding="utf-8") as f:
 
 setup(
     name="fluttercraft",
-    version="0.1.2.dev0",
+    version="0.1.2.dev1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
